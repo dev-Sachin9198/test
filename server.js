3969fa476e4fd52588cd3c61f1959cd23131d9bd
sachin yadav <ssachinyadav6537@gmail.com>
2026-09-01 22:36:40 +0530

    api methode

--- server.js
+++ server.js
@@ -1,17 +1,61 @@
-const http =require("http");
-const fs = require("fs");
+const express = require("express");
+const users = require("./MOCK_DATA.json");
+const MyServer = express();
+ const PORT = 8000;
 
-const MyServer=http.createServer((req ,res)=>{
-    const log = `${Date.now()}: New Request Recive/n`;
-    fs.appendFile("test.txt", log,(err , data)=>{
-      res.end("hey i am server");
-    });
-    
-
-
+ MyServer.get("/api/users", (req, res)=>{
+    res.json(users);
 });
 
+// MyServer.get("/api/users/:id", (req, res)=>{
+//     const {id} = req.params;
+//     const user = users.find((user)=>user.id === parseInt(id));
+//     if (!user) {
+//         return res.status(404).json({ error: "User not found" });
+//     }
+//     res.json(user);
+// });
 
-MyServer.listen(8000 ,()=>{
+MyServer.route("/api/users/:id").get((req, res)=>{
+    const {id} = req.params;
+    const user = users.find((user)=>user.id === parseInt(id));
+    if (!user) {
+        return res.status(404).json({ error: "User not found" });
+    }
+    res.json(user);
+}).post((req, res)=>{
+    const {id} = req.params;
+    const user = users.find((user)=>user.id === parseInt(id));
+    if (!user) {
+        return res.status(404).json({ error: "User not found" });
+    }
+    res.json(user);
+}).patch((req, res)=>{
+    const {id} = req.params;
+    const user = users.find((user)=>user.id === parseInt(id));  
+    if (!user) {
+        return res.status(404).json({ error: "User not found" });
+    }
+    res.json(user);
+}).delete((req, res)=>{
+    const {id} = req.params;
+    const userIndex = users.findIndex((user)=>user.id === parseInt(id));        
+    if (userIndex === -1) {
+        return res.status(404).json({ error: "User not found" });
+    }   
+    users.splice(userIndex, 1);
+    res.json({ message: "User deleted successfully" });
+});
+
+MyServer.get("/users",(req, res)=>{
+    const html =`
+    <ul>
+     ${users.map((user)=>`<li>${user.email}</li>`).join('')}
+    </ul>;
+    `
+    res.send(html);
+});
+
+MyServer.listen(PORT, ()=>{
     console.log("server start")
 });
